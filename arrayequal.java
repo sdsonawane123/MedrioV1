049286dfd191e1748c04bf74b5be733458533a5f
sdsonawane123 <131638530+sdsonawane123@users.noreply.github.com>
2023-04-24 10:36:06 +0530

    Add files via upload

---
+++ arrayequal.java
@@ -0,0 +1,22 @@
+package JavaBasic;
+
+import java.util.Arrays;
+
+public class arrayequal {
+	
+				
+				public static void main(String[] args) {
+					
+					
+					int[] ar1 = {43,56,76,78,32,56};
+					int[] ar2 = {56,87,98,4,3345,6};
+					
+					
+					boolean b =  Arrays.equals(ar1, ar2);
+					
+					
+				System.out.println(b);
+					
+				}
+
+}
